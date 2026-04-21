0b5cd3b0492d1e8cd1749421aa52bdeb67b822d0
lukeloveridgell-svg <lukeloveridge.ll@gmail.com>
2026-04-21 23:34:15 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Unity Barbershop</h1>
+      <p>Welcome to the shop 💈</p>
+    </div>
+  );
+}
